b4e7dd4b8914af14285ecaa2ae4f2a50f7ea43ab
Flávio Caetano <flavio@vieiracaetano.com>
2019-06-05 01:03:21 -0300

    fix(Homebrew): fix tarball checksum

--- HomebrewFormula/ios-simulator-gif.rb
+++ HomebrewFormula/ios-simulator-gif.rb
@@ -2,7 +2,7 @@ class IosSimulatorGif < Formula
   desc "Record a gif from your iOS simulator in a jiffy"
   homepage "https://github.com/fjcaetano/ios-simulator-gif"
   url "https://github.com/fjcaetano/ios-simulator-gif/archive/0.0.1.tar.gz"
-  sha256 "41e51c8a4d9dc21746d9ee01b956d245e156f636026c9ad01aa26bb7e738ed9b"
+  sha256 "d96d73f07fb10448287384f958fecc3e9917532479cfb068d782f8bac974e7f1"
 
   depends_on "ffmpeg"
 
