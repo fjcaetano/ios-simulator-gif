809839066962ca23b3fc47f3e1d1b524ed6e7497
Flávio Caetano <caetano@work.co>
2019-09-27 17:34:42 -0300

    chore(Upgrade): bump version to 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+# 1.1.0
+
+- Feat: Support multiple booted devices (#2)
+
+- Fix: Check if there are booted devices before recording (#1)
+- Fix: CLI basename on help command (#3)
+
 # 1.0.0
 
 - Feat: send arguments to script
--- HomebrewFormula/ios-simulator-gif.rb
+++ HomebrewFormula/ios-simulator-gif.rb
@@ -1,8 +1,8 @@
 class IosSimulatorGif < Formula
   desc "Record a gif from your iOS simulator in a jiffy"
   homepage "https://github.com/fjcaetano/ios-simulator-gif"
-  url "https://github.com/fjcaetano/ios-simulator-gif/archive/1.0.0.tar.gz"
-  sha256 "3a34bc612b7a7832bb46a30774b063845635fd56f1bd3744be65cbc0f4ceaa03"
+  url "https://github.com/fjcaetano/ios-simulator-gif/archive/1.1.0.tar.gz"
+  sha256 "79cba993e64e115e69b160553bbd2cd9b4a9c09cf93efb9b1ada5e8e7958bc72"
 
   depends_on "ffmpeg"
   depends_on "gnu-sed"
